2da5141dea7df9ed31cd7e66013c914e800c4848
dparker2 <crazdave@gmail.com>
2023-07-20 10:18:13 -0700

    day 10

---
+++ bin/day10.ml
@@ -0,0 +1,43 @@
+let signals = Array.make 241 1
+
+let instructions =
+  let rec rec_read_lines lines =
+    try rec_read_lines ((read_line () |> String.split_on_char ' ') :: lines)
+    with End_of_file -> lines
+  in
+  rec_read_lines [] |> List.rev
+
+let rec run idx = function
+  | [] -> ()
+  | instr :: rest -> (
+      let signal = signals.(idx - 1) in
+      match instr with
+      | [ "noop" ] ->
+          signals.(idx) <- signal;
+          run (idx + 1) rest
+      | [ "addx"; x ] ->
+          signals.(idx) <- signal;
+          signals.(idx + 1) <- signal + int_of_string x;
+          run (idx + 2) rest
+      | _ -> run idx rest)
+
+let strgth cycle = signals.(cycle - 1) * cycle
+
+let print_crt () =
+  for idx = 0 to 239 do
+    let pixel = idx mod 40 in
+    if pixel == 0 then print_newline ();
+    if signals.(idx) - 1 <= pixel && pixel <= signals.(idx) + 1 then
+      print_char '#'
+    else print_char '.'
+  done
+
+let () =
+  run 1 instructions;
+  let interesting_sum =
+    strgth 20 + strgth 60 + strgth 100 + strgth 140 + strgth 180 + strgth 220
+  in
+  Printf.printf "Part 1: %d\n" interesting_sum;
+  print_string "Part 2:";
+  print_crt ();
+  print_newline ()
