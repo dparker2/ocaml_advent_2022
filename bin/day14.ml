6dd312af9de9082d13a757fb3c29d46456771810
dparker2 <crazdave@gmail.com>
2023-07-25 23:42:39 -0700

    day 14 rewrite and part 2

--- bin/day14.ml
+++ bin/day14.ml
@@ -1,27 +1,32 @@
-let make_cave () = Array.make_matrix 170 100 '.'
-let cave = make_cave ()
+module Position = struct
+  type t = int * int
 
-let put_horizontal y x1 x2 =
-  for i = min x1 x2 to max x1 x2 do
-    cave.(y).(i - 450) <- '#'
-  done
+  let compare (x1, y1) (x2, y2) =
+    if compare y1 y2 == 0 then compare x1 x2 else compare y1 y2
+end
 
-let put_vertical x y1 y2 =
-  for j = min y1 y2 to max y1 y2 do
-    cave.(j).(x - 450) <- '#'
-  done
+module Positions = Set.Make (Position)
+
+type cave = { mutable sand : Positions.t; mutable rocks : Positions.t }
+
+let rec range a b = if a > b then range b a else List.init (1 + b - a) (( + ) a)
+let add_pos set pos = Positions.add pos set
+
+let add_shape x1 y1 x2 y2 set =
+  if x1 == x2 then
+    range y1 y2 |> List.map (fun y -> (x1, y)) |> List.fold_left add_pos set
+  else range x1 x2 |> List.map (fun x -> (x, y1)) |> List.fold_left add_pos set
 
 let ints_of_pair string =
   string |> String.split_on_char ',' |> List.map int_of_string
 
-let rec read_rocks = function
+let rec read_rocks set = function
   | s :: "->" :: e :: rest -> (
       match (ints_of_pair s, ints_of_pair e) with
       | [ x1; y1 ], [ x2; y2 ] ->
-          if x1 == x2 then put_vertical x1 y1 y2 else put_horizontal y1 x1 x2;
-          read_rocks (e :: rest)
+          read_rocks (add_shape x1 y1 x2 y2 set) (e :: rest)
       | _ -> invalid_arg "Invalid coord")
-  | [ _ ] -> ()
+  | [ _ ] -> set
   | _ -> invalid_arg "Something wrong"
 
 let read_input () =
@@ -31,30 +36,55 @@ let read_input () =
   in
   rec_read_lines []
 
-let _debug_cave () =
-  for y = 0 to Array.length cave - 1 do
-    for x = 0 to Array.length cave.(0) - 1 do
-      print_char cave.(y).(x)
+let _debug_cave cave =
+  for y = 0 to 165 do
+    for x = 450 to 550 do
+      if Positions.mem (x, y) cave.sand then print_char 'o'
+      else if Positions.mem (x, y) cave.rocks then print_char '#'
+      else print_char '.'
     done;
     print_newline ()
   done
 
-let simulate () =
-  let rec fall i j =
-    if cave.(i + 1).(j) == '.' then fall (i + 1) j
-    else if cave.(i + 1).(j - 1) == '.' then fall (i + 1) (j - 1)
-    else if cave.(i + 1).(j + 1) == '.' then fall (i + 1) (j + 1)
-    else cave.(i).(j) <- 'o'
-  in
-  let rec drop_sand i =
-    try
-      fall 0 50;
-      drop_sand i + 1
-    with Invalid_argument _ -> i
-  in
-  drop_sand 0
+let is_air cave pos =
+  not (Positions.mem pos cave.sand || Positions.mem pos cave.rocks)
+
+let rec part1_sand cave maxy x y =
+  if y > maxy then raise Exit
+  else
+    match
+      [ (x, y + 1); (x - 1, y + 1); (x + 1, y + 1) ]
+      |> List.filter (is_air cave)
+    with
+    | (x2, y2) :: _ -> part1_sand cave maxy x2 y2
+    | [] -> (x, y)
+
+let rec part2_sand cave floory x y =
+  if y == floory then (x, y)
+  else if Positions.mem (500, 0) cave.sand then raise Exit
+  else
+    match
+      [ (x, y + 1); (x - 1, y + 1); (x + 1, y + 1) ]
+      |> List.filter (is_air cave)
+    with
+    | (x2, y2) :: _ -> part2_sand cave floory x2 y2
+    | [] -> (x, y)
+
+let simulate cave sand_strategy =
+  try
+    while true do
+      cave.sand <- Positions.add (sand_strategy 500 0) cave.sand
+    done
+  with Exit -> ()
 
 let () =
-  read_input () |> List.iter read_rocks;
-  simulate () |> Printf.printf "Part 1: %d\n";
-  _debug_cave ()
+  let cave = { sand = Positions.empty; rocks = Positions.empty } in
+  cave.rocks <- read_input () |> List.fold_left read_rocks cave.rocks;
+  let _, maxy = Positions.max_elt cave.rocks in
+  simulate cave (part1_sand cave maxy);
+  cave.sand |> Positions.cardinal |> Printf.printf "Part 1: %d\n";
+  (* _debug_cave cave; *)
+  cave.sand <- Positions.empty;
+  simulate cave (part2_sand cave (maxy + 1));
+  (* _debug_cave cave; *)
+  cave.sand |> Positions.cardinal |> Printf.printf "Part 2: %d\n"
